97da8c54ae35a09bbbaa8f6c180e8311df31323b
krysnuvadga <tetechris20@gmail.com>
2019-04-29 20:06:16 -0400

    Adding a website blocker script

---
+++ website_blocker.py
@@ -0,0 +1,32 @@
+import time
+from datetime import datetime as dt
+
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect_url = "127.0.0.1"
+website_list=["www.facebook.com", "www.google.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8)  < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+
+        print("working hours....")
+
+        with open(hosts_path, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect_url+ "    "+ website +"\n")
+
+    else:
+        print("Fun hours....")
+
+        with open(hosts_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+
+    time.sleep(5)
